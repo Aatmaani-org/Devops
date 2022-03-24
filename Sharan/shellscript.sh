0e71cec2b3e500dbebe628fca11459f904185b9c
sooraj <soorajynaik036@gmail.com>
2022-03-24 15:15:18 +0000

    sharan

--- Sharan/shellscript.sh
+++ Sharan/shellscript.sh
@@ -7,7 +7,7 @@ echo  ----------  git clone  prod  -------
 git clone https://github.com/Aatmaani-org/Production.git
 
 cd Production
-a=`git log -1 --format="%H" `
+a=`git log -1 --format="%h" `
 echo $a
 aws ecr get-login-password --region us-west-2 | docker login --username AWS --password-stdin 883195043912.dkr.ecr.us-west-2.amazonaws.com
 docker build -t node-js .
