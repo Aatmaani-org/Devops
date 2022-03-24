e3db6aea07816cdf79180f9fa8fbdf7b12ce4601
sooraj <soorajynaik036@gmail.com>
2022-03-24 04:14:27 +0000

    update

--- Sharan/shellscript.sh
+++ Sharan/shellscript.sh
@@ -2,7 +2,7 @@
 echo git clone
 sudo  rm -rf *
 git clone https://github.com/Aatmaani-org/Devops.git
-
+pwd
 echo helm run
-cd /Devops/Sharan
+cd  /var/lib/jenkins/workspace/sharan/nodejs-qa/Devops/Sharan
 helm upgrade --install sharan-qa nodejs -n sharan-qa -f values-qa.yaml
