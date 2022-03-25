4a256ad7353053a0f76830a4e7b8d0712aad5c2b
sooraj <soorajynaik036@gmail.com>
2022-03-25 08:36:54 +0000

    update

--- Sharan/shellscript.sh
+++ Sharan/shellscript.sh
@@ -4,9 +4,10 @@ sudo  rm -rf *
 git clone https://github.com/Aatmaani-org/Devops.git
 
 echo  ----------  git clone  prod  -------
+mkdir  Production
+cd Production
 git clone https://github.com/Aatmaani-org/Production.git
 
-cd Production
 a=`git log -1 --format="%h" `
 echo $a
 aws ecr get-login-password --region us-west-2 | docker login --username AWS --password-stdin 883195043912.dkr.ecr.us-west-2.amazonaws.com
