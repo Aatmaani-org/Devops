baa17e6624a1cb5d619f3d29f28aef99bea0b116
Sooraj <soorajynaik036@gmail.com>
2022-03-29 09:45:25 +0000

    new update

--- Machendra/pullimageECR.sh
+++ Machendra/pullimageECR.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 echo "---- Pull the Image to the ECR ----"
 
-docker pull 883195043912.dkr.ecr.us-west-2.amazonaws.com/nodejs-repository-dev:node-app-$(git log -1 --format=%h)
+docker pull 883195043912.dkr.ecr.us-west-2.amazonaws.com/nodejs-repository-dev:dev-latest
+
+echo "---- Tagging the Image and Push Image to ECR ----"
+
+docker tag node-app:latest 883195043912.dkr.ecr.us-west-2.amazonaws.com/nodejs-repository-dev:qa-latest
+docker push 883195043912.dkr.ecr.us-west-2.amazonaws.com/nodejs-repository-dev:qa-latest
+
+echo "---- Creating the Pods using HELM ----"
+
+cd helm
+helm upgrade --install --set image.tag=qa-latest node-dev marvel -n qa -f values-qa.yaml
