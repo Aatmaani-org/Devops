74f213204ecb9f31408d55151d8ef4623318aec2
Sooraj <soorajynaik036@gmail.com>
2022-03-29 07:30:46 +0000

    new update

--- Machendra/pushimageToecr.sh
+++ Machendra/pushimageToecr.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "---- Pushing Image to the ECR ----"
-
+ls
 docker tag node-app:latest 883195043912.dkr.ecr.us-west-2.amazonaws.com/nodejs-repository-dev:node-app-$(git log -1 --format=%h)
 docker tag node-app:latest 883195043912.dkr.ecr.us-west-2.amazonaws.com/nodejs-repository-dev:node-demo
 aws ecr get-login-password --region us-west-2 | docker login --username AWS --password-stdin 883195043912.dkr.ecr.us-west-2.amazonaws.com
