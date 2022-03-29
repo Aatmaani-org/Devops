082f6d0cfc2d2797ed50fe6c0463c937f01e6a05
Sooraj <soorajynaik036@gmail.com>
2022-03-29 06:13:07 +0000

    update

--- Sharan/shellscript-dev/build.sh
+++ Sharan/shellscript-dev/build.sh
@@ -3,7 +3,6 @@ mkdir Production
 cd Production
 git clone https://github.com/Aatmaani-org/Production.git
 cd /var/lib/jenkins/workspace/sharan/nodejs-prod/Production/Production
-aws ecr get-login-password --region us-west-2 | docker login --username AWS --password-stdin 883195043912.dkr.ecr.us-west-2.amazonaws.com
 a=`git log -1 --format="%h" `
 echo $a
 aws ecr get-login-password --region us-west-2 | docker login --username AWS --password-stdin 883195043912.dkr.ecr.us-west-2.amazonaws.com
