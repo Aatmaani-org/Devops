d26169b48e80585c8f41980c29c8fc1ca53c066c
Sooraj <soorajynaik036@gmail.com>
2022-03-29 06:32:52 +0000

    new

--- Sooraj/build_push.sh
+++ Sooraj/build_push.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-/var/lib/jenkins/workspace/Sooraj/node-app/docker build -t new-nodejs --build-arg GIT_COMMIT=$(git log -1 --format=%h) .
 docker tag new-nodejs:latest 883195043912.dkr.ecr.us-west-2.amazonaws.com/new-nodejs:$(git log -1 --format=%h)
 docker tag new-nodejs:latest 883195043912.dkr.ecr.us-west-2.amazonaws.com/new-nodejs:dev-latest
 docker push 883195043912.dkr.ecr.us-west-2.amazonaws.com/new-nodejs:$(git log -1 --format=%h)
