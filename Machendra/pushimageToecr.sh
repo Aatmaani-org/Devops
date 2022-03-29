853a38b3703f9210695578c63bc2ba909f8973f2
Sooraj <soorajynaik036@gmail.com>
2022-03-29 07:23:17 +0000

    new update

--- Machendra/pushimageToecr.sh
+++ Machendra/pushimageToecr.sh
@@ -7,12 +7,13 @@ aws ecr get-login-password --region us-west-2 | docker login --username AWS --pa
 docker push 883195043912.dkr.ecr.us-west-2.amazonaws.com/nodejs-repository-dev:node-app-$(git log -1 --format=%h)
 docker push 883195043912.dkr.ecr.us-west-2.amazonaws.com/nodejs-repository-dev:node-demo
 cd
-
+ls
 echo "---- Creating pod using Helm ----"
 mkdir jenkins
-cd jenkins
-git clone https://github.com/Aatmaani-org/Devops.git
 ls
+#cd jenkins
+#git clone https://github.com/Aatmaani-org/Devops.git
+
 #cd /Devops/Machendra/helm/
 helm upgrade --install --set image.tag=node-app-$(git log -1 --format=%h) node-dev marvel -n dev -f values-dev.yaml
 #helm upgrade --install node-qa marvel -n qa -f values-qa.yaml
