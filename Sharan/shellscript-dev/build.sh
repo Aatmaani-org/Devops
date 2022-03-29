fb6583544fab80b975e970893e8522052c0e13ec
Sooraj <soorajynaik036@gmail.com>
2022-03-29 06:01:14 +0000

    up

--- Sharan/shellscript-dev/build.sh
+++ Sharan/shellscript-dev/build.sh
@@ -4,8 +4,9 @@ cd Production
 git clone https://github.com/Aatmaani-org/Production.git
 cd /var/lib/jenkins/workspace/sharan/nodejs-prod/Production/Production
 aws ecr get-login-password --region us-west-2 | docker login --username AWS --password-stdin 883195043912.dkr.ecr.us-west-2.amazonaws.com
+a=`git log -1 --format=%h`
 docker build -t new-nodejs .
-docker tag new-nodejs:latest 883195043912.dkr.ecr.us-west-2.amazonaws.com/new-nodejs:$(git log -1 --format="%h)
+docker tag new-nodejs:latest 883195043912.dkr.ecr.us-west-2.amazonaws.com/new-nodejs:$a
 docker tag new-nodejs:latest 883195043912.dkr.ecr.us-west-2.amazonaws.com/new-nodejs:dev-latest
-docker push 883195043912.dkr.ecr.us-west-2.amazonaws.com/new-nodejs:$(git log -1 --format="%h)
+docker push 883195043912.dkr.ecr.us-west-2.amazonaws.com/new-nodejs:$a
 docker push 883195043912.dkr.ecr.us-west-2.amazonaws.com/new-nodejs:dev-latest
