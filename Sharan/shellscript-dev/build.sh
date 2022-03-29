3727ab1eff761a10347d27294a1b97f4df2c0a0a
Sooraj <soorajynaik036@gmail.com>
2022-03-29 06:06:56 +0000

    up

--- Sharan/shellscript-dev/build.sh
+++ Sharan/shellscript-dev/build.sh
@@ -10,3 +10,13 @@ docker tag new-nodejs:latest 883195043912.dkr.ecr.us-west-2.amazonaws.com/new-no
 docker tag new-nodejs:latest 883195043912.dkr.ecr.us-west-2.amazonaws.com/new-nodejs:dev-latest
 docker push 883195043912.dkr.ecr.us-west-2.amazonaws.com/nodejs:$a
 docker push 883195043912.dkr.ecr.us-west-2.amazonaws.com/nodejs:dev-latest
+
+
+a=`git log -1 --format="%h" `
+echo $a
+aws ecr get-login-password --region us-west-2 | docker login --username AWS --password-stdin 883195043912.dkr.ecr.us-west-2.amazonaws.com
+docker build -t node-js .
+docker tag node-js:latest 883195043912.dkr.ecr.us-west-2.amazonaws.com/node-js:$a
+docker tag node-js:latest 883195043912.dkr.ecr.us-west-2.amazonaws.com/node-js:dev-latest
+docker push 883195043912.dkr.ecr.us-west-2.amazonaws.com/node-js:$a
+docker push 883195043912.dkr.ecr.us-west-2.amazonaws.com/node-js:dev-latest
