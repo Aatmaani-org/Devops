32ee87f22286a6743535505cb98f71bf3e9ce006
Sooraj <soorajynaik036@gmail.com>
2022-03-29 06:15:53 +0000

    update

--- Machendra/pushimageToecr.sh
+++ Machendra/pushimageToecr.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 echo "---- Pushing Image to the ECR ----"
 
 docker tag node-app:latest 883195043912.dkr.ecr.us-west-2.amazonaws.com/nodejs-repository-dev:node-app-$(git log -1 --format=%h)
