c44e2d7bb75eae0a0d07db9f099554e87ba538fe
Sooraj <soorajynaik036@gmail.com>
2022-03-29 11:00:41 +0000

    update

--- Sharan/shellscript-dev/qa.sh
+++ Sharan/shellscript-dev/qa.sh
@@ -3,10 +3,10 @@ echo ----- pull image ---------
 docker pull 883195043912.dkr.ecr.us-west-2.amazonaws.com/node-js:dev-latest
 
 echo -------  tag and push ----------
-docker tag node-js:qa-latest  883195043912.dkr.ecr.us-west-2.amazonaws.com/node-js:qa-latest
+docker tag 883195043912.dkr.ecr.us-west-2.amazonaws.com/node-js:dev-latest 883195043912.dkr.ecr.us-west-2.amazonaws.com/node-js:qa-latest
 docker push 883195043912.dkr.ecr.us-west-2.amazonaws.com/node-js:qa-latest
 
 
 echo ---------- helm run ------------
-helm upgrade --install sharan-qa nodejs -n sharan-qa -f values-qa.yaml
+helm upgrade --install --set image.tag=dev-latest sharan-qa nodejs -n sharan-qa -f values-qa.yaml
 
