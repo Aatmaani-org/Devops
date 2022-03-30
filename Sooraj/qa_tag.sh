53320486d878735ef41283444471ce89f1d90643
Sooraj <soorajynaik036@gmail.com>
2022-03-30 10:54:11 +0000

    new

--- Sooraj/qa_tag.sh
+++ Sooraj/qa_tag.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-a=`aws ecr describe-images --repository-name new-nodejs | grep -2 dev-latest | awk 'NR==1{print $1}' --output text`
-echo $a
+commit_id=`aws ecr describe-images --repository-name new-nodejs | grep -2 dev-latest | awk 'NR==1{print $1}' | tr -d '"',`
 image="883195043912.dkr.ecr.us-west-2.amazonaws.com/new-nodejs"
-docker pull $image:$a
-docker tag  $image:$a $image:qa-latest
+docker pull $image:$commit_id
+docker tag  $image:$commit_id $image:qa-latest
 docker push $image:qa-latest
 cd /var/lib/jenkins/workspace/Sooraj/node-app/warnerdevops/Devops/Sooraj/helm
-helm upgrade --install --set image.tag=$a warnerbro warnerbros -n qa -f values-qa.yaml
+helm upgrade --install --set image.tag=$commit_id warnerbro warnerbros -n qa -f values-qa.yaml
