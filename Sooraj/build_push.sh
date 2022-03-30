55c1a8eedb9a34fc0669b432685245877cdd83f7
Sooraj <soorajynaik036@gmail.com>
2022-03-30 05:38:17 +0000

    new

--- Sooraj/build_push.sh
+++ Sooraj/build_push.sh
@@ -8,3 +8,5 @@ docker push 883195043912.dkr.ecr.us-west-2.amazonaws.com/new-nodejs:dev-latest
 a=`aws ecr describe-images --repository-name new-nodejs \
 --query 'sort_by(imageDetails,& imagePushedAt)[-1].imageTags[1]' --output text`
 echo $a
+cd /var/lib/jenkins/workspace/Sooraj/node-app/warnerdevops/Devops/Sooraj/helm
+helm upgrade --install --set image.tag=$a warnerbro warnerbros -n dev -f values-dev.yaml
--- Sooraj/qa_tag.sh
+++ Sooraj/qa_tag.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
+a=`aws ecr describe-images --repository-name new-nodejs \
+--query 'sort_by(imageDetails,& imagePushedAt)[-1].imageTags[1]' --output text`
+echo $a
 image="883195043912.dkr.ecr.us-west-2.amazonaws.com/new-nodejs"
-docker pull $image:dev-latest
-docker tag  $image:dev-latest $image:qa-latest
+docker pull $image:$a
+docker tag  $image:$a $image:qa-latest
 docker push $image:qa-latest
+helm upgrade --install --set image.tag=$a warnerbro warnerbros -n qa -f values-dev.yaml
