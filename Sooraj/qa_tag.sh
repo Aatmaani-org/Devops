092abe858dd6acdd596219a776294e13607d2c5d
Sooraj <soorajynaik036@gmail.com>
2022-03-30 05:47:14 +0000

    new

--- Sooraj/qa_tag.sh
+++ Sooraj/qa_tag.sh
@@ -6,4 +6,5 @@ image="883195043912.dkr.ecr.us-west-2.amazonaws.com/new-nodejs"
 docker pull $image:$a
 docker tag  $image:$a $image:qa-latest
 docker push $image:qa-latest
+cd /var/lib/jenkins/workspace/Sooraj/node-app/warnerdevops/Devops/Sooraj/helm
 helm upgrade --install --set image.tag=$a warnerbro warnerbros -n qa -f values-dev.yaml
