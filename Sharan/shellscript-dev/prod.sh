4e9ef8a35eaa728b37c74c1c0fbf1133e0feeec8
Sooraj <soorajynaik036@gmail.com>
2022-03-30 11:11:25 +0000

    up

---
+++ Sharan/shellscript-dev/prod.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+aws ecr get-login-password --region us-west-2 | docker login --username AWS --password-stdin 883195043912.dkr.ecr.us-west-2.amazonaws.com
+echo ----- pull image ---------
+docker pull 883195043912.dkr.ecr.us-west-2.amazonaws.com/node-js:qa-latest
+echo -------  tag and push ----------
+docker tag 883195043912.dkr.ecr.us-west-2.amazonaws.com/node-js:qa-latest 883195043912.dkr.ecr.us-west-2.amazonaws.com/node-js:prod-latest
+docker push 883195043912.dkr.ecr.us-west-2.amazonaws.com/node-js:prod-latest
+latest_id=`aws ecr describe-images --repository-name new-nodejs | grep -2 dev-latest |awk 'NR==1{print $1}'| tr -d '"',`
+echo $latest_id
+echo ---------- helm run ------------
+helm upgrade --install --set image.tag=$latest_id gost nodejs -n prod -f values-prod.yaml
+
