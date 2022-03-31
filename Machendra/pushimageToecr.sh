6d231ee99c15c9dc436555855b54abd0ee10f73b
Sooraj <soorajynaik036@gmail.com>
2022-03-31 03:58:32 +0000

    new update

--- Machendra/pushimageToecr.sh
+++ Machendra/pushimageToecr.sh
@@ -12,7 +12,9 @@ aws ecr get-login-password --region us-west-2 | docker login --username AWS --pa
 docker push 883195043912.dkr.ecr.us-west-2.amazonaws.com/nodejs-repository-dev:$(git log -1 --format=%h)
 docker push 883195043912.dkr.ecr.us-west-2.amazonaws.com/nodejs-repository-dev:dev-latest
 cd
-
+ls
+pwd
+ls
 commit_id=`aws ecr describe-images --repository-name nodejs-repository-dev --output json | grep -1 dev-latest | awk 'NR==1{print $1}' | tr -d '"',`
 
 echo "---- Creating pod using Helm ----"
