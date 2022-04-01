942bfb2b596b7d85d52ae6793341283400373c52
Sooraj <soorajynaik036@gmail.com>
2022-04-01 11:06:47 +0000

    new

--- Sharan/build.sh
+++ Sharan/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-git_commit=`git log -1 --format="%h" `
+git_commit=`git log -1 --format="%h"`
 echo $git_commit
 aws ecr get-login-password --region us-west-2 | docker login --username AWS --password-stdin 883195043912.dkr.ecr.us-west-2.amazonaws.com
 docker build -t node-js .
@@ -7,3 +7,4 @@ docker tag node-js:latest 883195043912.dkr.ecr.us-west-2.amazonaws.com/node-js:$
 docker tag node-js:latest 883195043912.dkr.ecr.us-west-2.amazonaws.com/node-js:dev-latest
 docker push 883195043912.dkr.ecr.us-west-2.amazonaws.com/node-js:$git_commit
 docker push 883195043912.dkr.ecr.us-west-2.amazonaws.com/node-js:dev-latest
+helm upgrade --install --set image.tag=$git_commit nodejss nodejs -n dev -f values-dev.yaml
