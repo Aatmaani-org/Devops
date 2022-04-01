63dc449962cdbb4bf347e41ebad2998f2f2a8188
Sooraj <soorajynaik036@gmail.com>
2022-04-01 11:28:57 +0000

    up

--- Sharan/shellscript-dev/qa.sh
+++ Sharan/shellscript-dev/qa.sh
@@ -5,7 +5,7 @@ docker pull 883195043912.dkr.ecr.us-west-2.amazonaws.com/node-js:dev-latest
 echo -------  tag and push ----------
 docker tag 883195043912.dkr.ecr.us-west-2.amazonaws.com/node-js:dev-latest 883195043912.dkr.ecr.us-west-2.amazonaws.com/node-js:qa-latest
 docker push 883195043912.dkr.ecr.us-west-2.amazonaws.com/node-js:qa-latest
-latest_id=`aws ecr describe-images --repository-name nodejs | grep -2 dev-latest |awk 'NR==1{print $1}'| tr -d '"',`
+latest_id=`aws ecr describe-images --repository-name node-js | grep -2 qa-latest |awk 'NR==1{print $1}'| tr -d '"',`
 echo $latest_id
 echo ---------- helm run ------------
 helm upgrade --install --set image.tag=$latest_id gost nodejs -n qa -f values-qa.yaml
