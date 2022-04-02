4a8c7f6de015e2bc44a1b1ca48aff6f652de57c2
Soorajnaik <98380496+Soorajnaik@users.noreply.github.com>
2022-04-02 15:59:29 +0530

    Master (#134)
    * new
    
    * new

--- Sooraj/prod_tag.sh
+++ Sooraj/prod_tag.sh
@@ -3,7 +3,7 @@ commit_id=`aws ecr describe-images --repository-name new-nodejs | grep -3 qa-lat
 image="883195043912.dkr.ecr.us-west-2.amazonaws.com/new-nodejs"
 
 echo "------Pulling the image using commit_id having the qa_latest tag------"
-
+aws ecr get-login-password --region us-west-2 | docker login --username AWS --password-stdin 883195043912.dkr.ecr.us-west-2.amazonaws.com
 docker pull $image:$commit_id
 
 echo "------Tagging the image with prod-latest and pushing it to ECR------"
